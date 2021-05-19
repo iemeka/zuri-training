9720a81d4a85046e3168928c84996524cd8213e0
iemeka <mekstines@gmail.com>
2021-05-19 14:25:19 +0100

    simple functions

--- algorithm/index.js
+++ algorithm/index.js
@@ -11,21 +11,22 @@ function errorMessage(inputValue, inputValueType) {
   return `invalid parameter : ${inputValueString}`; // YUGIHO
 }
 
-
-// VALIDATE TYPE. 
+// VALIDATE TYPE.
 // RETURNS TRUE IF A TYPE IS A "NUMBER" OR TYPE IS A STRING(BUT NOT AN EMPTY STRING(" ") AND NOT A STRING THAT CONTAINS LETTERS OR OTHER CHARACTERS("ABC123.?")
 function validateType(fahrType, fahrValue) {
   if (
     fahrType === "number" ||
-    (fahrType === "string" && fahrValue.trim().length > 0 && !isNaN(Number(fahrValue)))
+    (fahrType === "string" &&
+      fahrValue.trim().length > 0 &&
+      !isNaN(Number(fahrValue)))
   ) {
     return true;
   }
   return false;
 }
 
-// RETURNS REAL TYPE OF PARAMETER  
-// FOR TYPE ARRAY - [OBJECT ARRAY], FOR TYPE STRING - [OBJECT STRING], FOR OBJECT - [OBJECT OBJECT], 
+// RETURNS REAL TYPE OF PARAMETER
+// FOR TYPE ARRAY - [OBJECT ARRAY], FOR TYPE STRING - [OBJECT STRING], FOR OBJECT - [OBJECT OBJECT],
 // ..ETC.WE JUST NEED TO EXTRACT THE SUBSTRING "ARRAY", "STRING", "OBJECT"..ETC.
 function getType(fahrValue) {
   const internalType = Object.prototype.toString.call(fahrValue);
@@ -50,9 +51,37 @@ function convertFahrToCelsius(fahr) {
 }
 
 //HOW TO RUN:
-console.log(convertFahrToCelsius("23dd"));
+const TOCELSIUSparam = {test:[1,2,3],try:"nothing"};
+console.log(convertFahrToCelsius(TOCELSIUSparam));
 
 // 2. Write a function named "checkYuGiOh" that takes a number, n, as an argument, creates an array of numbers from 1 to n and replaces multiples of 2, 3, and 5 with "yu", "gi" and "oh", then returns the resulting array.
+
+// FUNCTIONS TO HELP ONLY checkYuGiOh FUNCTION
+
+function isEmptyString(stringHolder, stringValue) {
+  const lengthOfStringHolder = stringHolder.length;
+  if (stringValue !== null) {
+    if (lengthOfStringHolder !== 0) {
+      stringHolder += "-";
+    }
+    return stringHolder + stringValue;
+  }
+  return lengthOfStringHolder === 0 ? true : false;
+}
+
+function accumulator(i) {
+  const table = { yu: 2, gi: 3, oh: 5 };
+  const stringsTojoin = ["yu", "gi", "oh"];
+  let stringAccumulator = "";
+  for (let stringValue of stringsTojoin) {
+    if (i % table[stringValue] === 0) {
+      stringAccumulator = isEmptyString(stringAccumulator, stringValue);
+    }
+  }
+  return stringAccumulator;
+}
+
+// MAIN FUNCTION
 
 function checkYuGiOh(inputParam) {
   const paramType = getType(inputParam);
@@ -60,27 +89,12 @@ function checkYuGiOh(inputParam) {
   if (isValidType) {
     const inputNumber = Number(inputParam);
     const arr = [];
-    for (let i = 1; i < inputNumber + 1; i++) {
-      let stringHolder = "";
-      if (i % 2 === 0) stringHolder += "yu";
-      if (i % 3 === 0) {
-        if (stringHolder.length === 0) {
-          stringHolder += "gi";
-        } else {
-          stringHolder += "-gi";
-        }
-      }
-      if (i % 5 === 0) {
-        if (stringHolder.length === 0) {
-          stringHolder += "oh";
-        } else {
-          stringHolder += "-oh";
-        }
-      }
-      if (stringHolder.length === 0) {
+    for (let i = 1; i <= inputNumber; i++) {
+      let accumulatedString = accumulator(i);
+      if (isEmptyString(accumulatedString, null)) {
         arr.push(i);
       } else {
-        arr.push(stringHolder);
+        arr.push(accumulatedString);
       }
     }
     return arr;
@@ -89,7 +103,5 @@ function checkYuGiOh(inputParam) {
 }
 
 //HOW TO RUN:
-console.log(checkYuGiOh("30"));
-
-
-
+const YUGIHOparam = {test:[1,2,3],try:"nothing"};
+console.log(checkYuGiOh(YUGIHOparam));
